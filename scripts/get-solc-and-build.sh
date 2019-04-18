c43b827d3a1860442fd9a2db4c380033922e3976
Francisco Giordano <frangio.1@gmail.com>
2019-04-18 17:14:52 -0300

    switch to bash for -v operator

--- scripts/get-solc-and-build.sh
+++ scripts/get-solc-and-build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
 # We use $DEPLOY_URL to detect the Netlify environment.
-if [ -v DEPLOY_URL ]; then
+if [[ -v DEPLOY_URL ]]; then
   : ${NETLIFY_CACHE_DIR="/opt/buildhome/cache"}
 else
   : ${NETLIFY_CACHE_DIR="$PWD"}
@@ -11,8 +11,7 @@ fi
 
 export SOLC_PATH="$NETLIFY_CACHE_DIR/solc"
 
-if [ ! -x "$SOLC_PATH" ]
-then
+if [[ ! -x "$SOLC_PATH" ]]; then
   curl -L "https://github.com/ethereum/solidity/releases/download/v0.5.7/solc-static-linux" > "$SOLC_PATH"
   chmod +x "$SOLC_PATH"
 fi
